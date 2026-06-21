484cbacd040e418b11fea0bc45abe7451209ddf0
tekutekunikki <tekutekunikki03@gmail.com>
2026-06-21 12:03:50 +0900

    Align Android JVM targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,17 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
