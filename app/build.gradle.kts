bd78fd7ba1a73a2796ac22b24e2643c5f4abb407
tekutekunikki <tekutekunikki03@gmail.com>
2026-07-17 23:33:08 +0900

    Add hydration splash screen (#72)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ kotlin {
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
